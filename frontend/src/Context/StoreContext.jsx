6fc6a37839422d0ccce87ecb3e0a52f8cd3c4e15
wasimKhan <wk09908@gmail.com>
2024-06-12 12:06:14 +0530

    Update orderController.js and StoreContext.jsx, delete ReviewForm components

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -6,7 +6,7 @@ const StoreContextProvider = (props) => {
 
     const [cartItems, setCartItems] = useState({});
     const [token, setToken] = useState("");
-    const url = "http://localhost:4000"
+    const url = "https://bhojan-backend-3.onrender.com/"
     const [food_list, setFoodlist] = useState([]);
     const [showBox, setShowBox] = useState(false);
     const [itemId, setItemId] = useState("");
