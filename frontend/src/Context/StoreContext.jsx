8fd616656145be5c299c94e006ccee54e3818900
wasimKhan <wk09908@gmail.com>
2024-06-28 21:28:28 +0530

    changes done

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -6,7 +6,7 @@ export const StoreContext = createContext(null);
 const StoreContextProvider = (props) => {
     const [cartItems, setCartItems] = useState({});
     const [token, setToken] = useState("");
-     const url = "http://localhost:4000"
+    const url = "https://bhojan-backend-7.onrender.com"; 
     const [food_list, setFoodlist] = useState([]);
     const [showBox, setShowBox] = useState(false);
     const [itemId, setItemId] = useState("");
